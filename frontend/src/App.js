b5fd361d01d80959b1d9790056ec5821aabe7c51
ShaolongYao <shaolongyao@gmail.com>
2021-12-19 21:08:53 -0500

    refactor: rename my title and main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is Shaolong Yao portfolio Website
         </a>
       </header>
     </div>
