16305448dac61010efe5f476dc265135eadafb25
ShaolongYao <shaolongyao@gmail.com>
2021-12-20 23:05:30 -0500

    feat: ampify auth&add

---
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "shaolongyaoAuth": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "shaolongyao": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s34a56cdac": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
