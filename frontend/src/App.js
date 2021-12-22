c99286e6a089ecf4a19fca7a2ccb1ae1a53a5da0
ShaolongYao <shaolongyao@gmail.com>
2021-12-21 21:34:12 -0500

    feat: adding router Mui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,12 @@
 import './App.css';
 
-import logo from './logo.svg';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello,I am Shaolong Yao</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This is Shaolong Yao portfolio Website
-        </a>
-      </header>
+Hello
     </div>
   );
 }
 
 export default App;
+ 
