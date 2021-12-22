986c299c6db4a65e6ae86c3dbb78f926179d35e5
ShaolongYao <shaolongyao@gmail.com>
2021-12-21 21:53:13 -0500

    feat: Push testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
+import {Typography} from "@mui/material";
+
 function App() {
   return (
     <div className="App">
-Hello
+<Typography variant="h5" gutterBottom component="div">
+        h5. Heading
+      </Typography>
     </div>
   );
 }
